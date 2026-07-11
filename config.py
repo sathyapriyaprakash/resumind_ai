e9f178ce57654defa2514621cd7d9a22754a60e2
Sathyapriya Prakash <sathyapriyaprakash09@gmail.com>
2026-07-11 16:42:06 +0530

    Support Streamlit Secrets

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Read from .env (local)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# If not found, read from Streamlit Secrets (Cloud)
+if not GEMINI_API_KEY:
+    GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY")
+
+if not GEMINI_API_KEY:
+    raise ValueError("Missing GEMINI_API_KEY in environment.")
+
 MODEL_NAME = "gemini-2.5-flash"
+
 UPLOAD_DIR = "assets/uploads"
 
-if not os.path.exists(UPLOAD_DIR):
-    os.makedirs(UPLOAD_DIR)
+os.makedirs(UPLOAD_DIR, exist_ok=True)
